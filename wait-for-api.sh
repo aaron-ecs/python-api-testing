db67085fe164e0f97d101eebb83f6b64d15db97b
aaron-ecs <aaron.williams@ecs-digital.co.uk>
2019-02-12 17:44:29 +0000

    Message print fix in wait bash script

--- wait-for-api.sh
+++ wait-for-api.sh
@@ -5,14 +5,16 @@ attempt_counter=0
 max_attempts=12
 
 until $(curl --output /dev/null --silent --head --fail http://api:8080); do
-    if [[ ${attempt_counter} -eq ${max_attempts} ]];then
-      echo "user exercises rest API reached!"
+    if [[ ${attempt_counter} -eq ${max_attempts} ]]; then
+      echo "User exercises rest API cannot reached!"
       exit 1
     fi
 
-    printf '.'
+    printf "."
     attempt_counter=$(($attempt_counter+1))
     sleep 5
 done
+
+echo "\nUser exercises rest API has been reached! Starting tests."
 
 behave
